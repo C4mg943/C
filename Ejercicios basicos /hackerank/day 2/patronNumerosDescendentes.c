f7147bbcde86cd5b993e8f645374eb5786ad6e3c
Camilo Monsalve <homeless_e@proton.me>
2025-06-08 13:07:56 -0500

    corr

--- Ejercicios basicos /hackerank/day 2/patronNumerosDescendentes.c
+++ Ejercicios basicos /hackerank/day 2/patronNumerosDescendentes.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
+int main() {
     int n;
-        scanf("%d",&n);
-        int size = 2 * n - 1;
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    int Mini = i;
-                    if (j < Mini) Mini = j;
-                    if (size - 1 - i < Mini) Mini = size - 1 - i;
-                    if (size - 1 - j < Mini) Mini = size - 1 - j;
+    scanf("%d", &n);
 
-                    printf("%d", n - Mini);
+    int size = 2 * n - 1;
 
-                    if (j < size - 1)
-                        printf(" ");
-                }
-                printf("\n");
-            }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            // Calculamos la distancia mínima al borde
+            int min_dist = i;
+            if (j < min_dist) min_dist = j;
+            if (size - 1 - i < min_dist) min_dist = size - 1 - i;
+            if (size - 1 - j < min_dist) min_dist = size - 1 - j;
+
+            // Valor en la celda = n - min_dist
+            printf("%d", n - min_dist);
+
+            // Espacio entre números (excepto al final de la fila)
+            if (j < size - 1)
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
